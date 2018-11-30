a5ade912ada8ef07e57512bd30d49c190f8c2235
bhuvanp96 <40825816+bhuvanp96@users.noreply.github.com>
2018-11-30 20:30:47 +0530

    DBMS Mini Project
    Implemented with basic CRUD operations (Create, Update , retrieve and delete)

---
+++ README.md
@@ -0,0 +1,4 @@
+Simple Blog part 1: The Build
+=============
+
+These files acompany the tutorial: [Creating a blog from scratch with PHP](http://daveismyname.com/creating-a-blog-from-scratch-with-php-bp)
---
+++ includes/config.php
@@ -0,0 +1,44 @@
+<?php
+ob_start();
+session_start();
+
+//database credentials
+define('DBHOST','localhost');
+define('DBUSER','root');
+define('DBPASS','');
+define('DBNAME','blog_members');
+
+$db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+//set timezone
+date_default_timezone_set('Europe/London');
+
+//load classes as needed
+function __autoload($class) {
+   
+   $class = strtolower($class);
+
+	//if call from within assets adjust the path
+   $classpath = 'classes/class.'.$class . '.php';
+   if ( file_exists($classpath)) {
+      require_once $classpath;
+	} 	
+	
+	//if call from within admin adjust the path
+   $classpath = '../classes/class.'.$class . '.php';
+   if ( file_exists($classpath)) {
+      require_once $classpath;
+	}
+	
+	//if call from within admin adjust the path
+   $classpath = '../../classes/class.'.$class . '.php';
+   if ( file_exists($classpath)) {
+      require_once $classpath;
+	} 		
+	 
+}
+
+$user = new User($db); 
+?>
